b72e449394d918166cc9330224b78ff455034ade
luqi2000 <luqmanmohammad09@gmail.com>
2023-03-27 11:06:21 +0100

    first-commit

---
+++ src/components/MyComponent.jsx
@@ -0,0 +1,11 @@
+function MyComponent() {
+  return (
+    <ul>
+      <li>Primo elemento</li>
+      <li>secondo elemento</li>
+      <li>terzo elemento</li>
+    </ul>
+  );
+}
+
+export default MyComponent;
